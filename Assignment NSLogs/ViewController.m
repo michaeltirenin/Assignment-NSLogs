1c0dd1cfcb120e418c71ef409de61d47798d2e79
Michael Tirenin <michaeltirenin@Michaels-MacBook-Pro-2.local>
2014-05-07 11:57:41 -0700

    Updating 2 files.

--- Assignment NSLogs/ViewController.m
+++ Assignment NSLogs/ViewController.m
@@ -10,6 +10,14 @@
 
 @interface ViewController ()
 
+- (IBAction)logButtonMessageToConsole:(UIButton *)sender;
+
+- (IBAction)logSwitchMessageToConsole:(UISwitch *)sender;
+
+- (IBAction)logSliderMessageToConsole:(UISlider *)sender;
+
+- (IBAction)goToSecondView:(UIButton *)sender;
+
 @end
 
 @implementation ViewController
@@ -24,6 +32,38 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// Button logs a message to the console when it is tapped.
+- (IBAction)logButtonMessageToConsole:(UIButton *)sender
+{
+    NSLog(@"Button was tapped.");
+}
+
+// Switch logs a message to the console when it is toggled.
+- (IBAction)logSwitchMessageToConsole:(UISwitch *)sender
+{
+//    NSLog(@"Switch was toggled.");
+    if ([sender isOn]) {
+        NSLog(@"Switch was toggled on.");
+    } else {
+        NSLog(@"Switch was toggled off.");
+    }
+}
+
+// Slider logs a message to the console with the current slider value,
+// but only when the value is greater than 0.5.
+- (IBAction)logSliderMessageToConsole:(UISlider *)sender
+{
+    if (sender.value > 0.5) {
+        NSLog(@"Slider value is %f.", sender.value);
+    }
+}
+
+// Challenge: Second button presents the 2nd view controller when tapped.
+- (IBAction)goToSecondView:(UIButton *)sender
+{
+    NSLog(@"Next button was tapped, going to 2nd view.");
 }
 
 @end
